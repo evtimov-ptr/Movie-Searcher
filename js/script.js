e7098ac6b96410cb70e69f0fe957a31270b700a3
Evtim Evtimov <evtimtrago@gmail.com>
2023-03-04 13:48:34 +0200

    Minor improvements
    1. Added Type of the movie either is it a Movie or Series.
    2. Added Time (how long the movie is).
    3. Some minor CSS adjustments.

--- js/script.js
+++ js/script.js
@@ -28,9 +28,13 @@ const getMovie = () => {
               <h4>${data.imdbRating}</h4>
               </div>
               <div class="details">
-                <span>${data.Rated}</span>
-                <span>${data.Year}</span>
-                <span>${data.Runtime}</span>
+                <span>Rated: ${data.Rated}</span>
+                <span>Year: ${data.Year}</span>
+                <span>Time: ${data.Runtime}</span>
+                <span>Type: ${
+                  data.Type.charAt(0).toUpperCase() + data.Type.slice(1)
+                }</span>
+             
                 </div>
                 <div class="genre">
                   <div>${data.Genre.split(",").join("</div><div>")}</div>
@@ -53,4 +57,4 @@ const getMovie = () => {
 };
 
 searchBtn.addEventListener("click", getMovie);
-window.addEventListener("load", getMovie);
+// window.addEventListener("load", getMovie);
